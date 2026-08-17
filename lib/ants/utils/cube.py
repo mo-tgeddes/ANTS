39e5229ac0612a1f7baaf2da613968e82a86f2d2
Theo Geddes <theo.geddes@metoffice.gov.uk>
2026-08-17 11:49:02 +0100

    Add logic comments

--- lib/ants/utils/cube.py
+++ lib/ants/utils/cube.py
@@ -1171,8 +1171,13 @@ def copy_metadata_attributes(
     """
 
     for attribute in metadata_to_copy:
+        # Only copy an attribute if it exists in the reference cube.
         if attribute in reference.attributes:
+            # If the attribute already exists in the source cube
             if attribute in source.attributes:
+                # If the attributes are the same then nothing needs to be done.
+                # If the attributes are not the same then both names must be prepended
+                # to ensure that the metadata is traceable.
                 if source.attributes[attribute] != reference.attributes[attribute]:
                     source.attributes[attribute] = (
                         source.name()
@@ -1184,6 +1189,7 @@ def copy_metadata_attributes(
                         + reference.attributes[attribute]
                     )
             else:
+                # If the attribute does not exist in the source cube
                 source.attributes[attribute] = (
                     reference.name() + " = " + reference.attributes[attribute]
                 )
